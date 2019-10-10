4f3d58ebbdaec3413689800b4423d1ba473c7c32
Jinsoo Heo <dev@koriel.kr>
2019-10-10 22:13:54 +0900

    리다이렉트되기 전 상태 코드를 체크
    URL의 GET 응답이 301, 302등 리다이렉트라면 리다이렉트된 페이지의 상태 코드(200)이 아니라 그 이전의 상태 코드(301, 302)를 체크해야한다.

--- frypan.py
+++ frypan.py
@@ -16,12 +16,20 @@ def isSpam(content, spamLinkDomains, redirectionDepth):
     except requests.exceptions.TooManyRedirects as exception:
         response = exception.response
 
-    if response.status_code in (301, 302):
+    if response.history:
+        status_code = response.history[0].status_code
+    else:
+        status_code = response.status_code
+
+    if status_code in (301, 302):
         return isSpam(response.url, spamLinkDomains, redirectionDepth - 1)
-    elif response.status_code == 200:
+    elif status_code == 200:
         webpage = html.fromstring(response.content)
         hrefs = webpage.xpath('//a/@href')
 
-        return isSpam(hrefs[0], spamLinkDomains, redirectionDepth - 1)
+        if hrefs:
+            return isSpam(hrefs[0], spamLinkDomains, redirectionDepth - 1)
+        else:
+            return False
     else:
         return False
